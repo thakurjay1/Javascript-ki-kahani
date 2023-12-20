6c2a3416e46c6d8105d425ec4247ff2226640908
Jay Thakur <92983729+thakurjay1@users.noreply.github.com>
2023-12-20 13:07:56 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,19 @@
+const accountId = 14421
+let accountEmail = "jay123@gmail.com"
+var accountPassword = "145534"
+accountCity = "Agra"
+
+acountId = 2
+accountEmail="rishabh@gmail.com"
+accountPassword="755273"
+accountCity="Noida"
+
+let accountState;
+
+/*
+Prfer not to use var
+because of issues in block scope and functional scope
+*/
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
