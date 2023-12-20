52adc1320c75480f595a13587eb5f5b677889c02
Jay Thakur <92983729+thakurjay1@users.noreply.github.com>
2023-12-20 18:59:26 +0000

    Conversion of types part2

--- 01_basics/03_conversionOperation.js
+++ 01_basics/03_conversionOperation.js
@@ -47,4 +47,39 @@ console.log(typeof stringKey)
 
 
 
+// *********************Operations*************************
 
+console.log("++++++++++++++++++++++++++++++")
+let value = 3
+let negValue = -value
+console.log(negValue)
+
+
+let str1 = "hello"
+let str2 = " Jay"
+str3 = str1+str2
+//console.log(str3)
+
+
+// console.log(1+"2")
+// console.log("1"+2)
+// console.log("1"+"2")
+// console.log("1"+2+2)
+// console.log(1+2+"2")
+// console.log(1+2+"3"+4+5)
+
+// console.log(true);
+// console.log(+true);
+// console.log(-true);
+// console.log(+"")
+// console.log(-"")
+
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.table([num1, num2, num3])
+ 
+let gameCounter = 100
+gameCounter++
+console.log(gameCounter);
+++gameCounter
+console.log(gameCounter);
