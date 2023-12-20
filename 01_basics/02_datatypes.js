02a590119e5efde96be5330bece0ab50be1d2194
Jay Thakur <92983729+thakurjay1@users.noreply.github.com>
2023-12-20 13:06:33 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,19 @@
+"use strict" //treat all JS code as newer version
+
+// alert(3+3) // We are using node js, not browser and javascript execution engine is inside browser
+
+
+// number -> 2 to power 53
+// bigint
+// string-> ""
+// boolean -> true or false
+// null -> standalone value
+// undefined -> 
+// symbol -> unique
+
+//object
+
+console.log(typeof 53)
+console.log(typeof "")
+console.log(typeof null) // type of null is Object
+console.log(typeof undefined)
