c4e7ce4d7a309da88f9f5b94c8d5d4625189c92b
Jay Thakur <92983729+thakurjay1@users.noreply.github.com>
2023-12-20 18:12:35 +0000

    Conversion of datatypes

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,50 @@
+let score = true
+// let score = undefined
+// let score = null
+// let score = "86abc"
+// let score = "57"
+
+console.log(typeof score)
+console.log(typeof (score))
+
+let valueInNumber = Number(score);
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+// "33" -> 33
+// "33abc" -> Nan
+// true -> 1
+// false -> 0
+// null -> 0
+
+
+
+//let isLoggedIn = 1                   //true
+//let isLoggedIn = 0                   //false
+// let isLoggedIn = ""                 //false
+// let isLoggedIn = "Jay Thakur"       //true          
+//let isLoggedIn = 243                 //true
+//let isLoggedIn = -1                  //true
+// let isLoggedIn = null               //false
+let isLoggedIn = undefined           //false
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+console.log(booleanIsLoggedIn)
+
+
+
+// let key = 12               //String
+// let key = undefined        //String
+//let key = true              //String
+let key = null                //String
+
+
+
+let stringKey = String(key)
+
+console.log(stringKey)
+console.log(typeof stringKey)
+
+
+
+
